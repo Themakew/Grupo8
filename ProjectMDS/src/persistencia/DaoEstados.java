e3a1096dd649ca65a49be28f9c7bf16e48f1330e
lucienemlf <marialucienefelix@gmail.com>
2013-10-15 02:03:17 -0300

    resgatando os parametros no banco

--- ProjectMDS/src/persistencia/DaoEstados.java
+++ ProjectMDS/src/persistencia/DaoEstados.java
@@ -14,22 +14,72 @@ public class DaoEstados extends Dao {
 
 	}
 
-	public List<UnidadesFederativas> buscaUF(String ano, String tipo)
+	public List<UnidadesFederativas> buscaUF(String anoUF, String tipo)
 			throws Exception {
+		
+		int ano = Integer.parseInt(anoUF);
 
 		List<UnidadesFederativas> uf = new ArrayList<UnidadesFederativas>();
 
 		try {
 
 			Connection conexao = gerarConexao();
+		
 
 			String query = "select regiao, opcao, ano"
 					+ano+" from dldufabsoluto2001_2009 union select regiao, opcao, ano"+ano+" from dldufabsoluto2001_2009";
-
+			
+			//executa uma cláusula SQL
 			Statement stm = (Statement) conexao.createStatement();
+			
+			// executa um select- permite navegar pelos registros
 			ResultSet resultadoUF = (ResultSet) stm.executeQuery(query);
-			resultadoUF.getString(1);
-			uf.add(new UnidadesFederativas());
+			
+			while(resultadoUF.next()){
+				
+				//coluna pais
+				String nome = resultadoUF.getString(1);
+				
+				//coluna opcao
+				String opcao = resultadoUF.getString(2);
+				
+				if(ano == 2001){
+					String data = resultadoUF.getString(3);
+					
+				}else if(ano == 2002){
+					String data = resultadoUF.getString(4);
+					
+				}else if(ano == 2003){
+					String data = resultadoUF.getString(5);
+					
+				}else if(ano == 2004){
+					String data = resultadoUF.getString(6);
+					
+				}else if(ano == 2005){
+					String data = resultadoUF.getString(7);
+					
+				}else if(ano == 2006){
+					String data = resultadoUF.getString(8);
+					
+				}else if(ano == 2007){
+					String data = resultadoUF.getString(9);
+					
+				}else if(ano == 2008){
+					String data = resultadoUF.getString(10);
+					
+				}else if(ano == 2009){
+					String data = resultadoUF.getString(11);
+					
+				}else{
+					System.out.println("Opção invalida");
+				}
+				
+				
+				uf.add(new UnidadesFederativas(nome , data , opcao,tipo));
+				
+			}
+
+			
 			stm.close();
 			fecharConexao();
 
